323cf5a7de963f102166340634c551933f1d1200
Alessio Vulpinari <alessiovulpinari99@gmail.com>
2024-07-01 14:31:23 +0200

    Initial commit

---
+++ src/main/java/alessiovulpinari/u2_w3_d2/U2W3D2Application.java
@@ -0,0 +1,13 @@
+package alessiovulpinari.u2_w3_d2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2W3D2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2W3D2Application.class, args);
+	}
+
+}
---
+++ src/test/java/alessiovulpinari/u2_w3_d2/U2W3D2ApplicationTests.java
@@ -0,0 +1,13 @@
+package alessiovulpinari.u2_w3_d2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2W3D2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
